3123e8030c23b4ed6456a7a17c3e150002b9ab33
Jacob <bayswaterpc@gmail.com>
2021-03-14 18:18:06 -0500

    Filling Central Panel

--- src/app.rs
+++ src/app.rs
@@ -72,26 +72,59 @@ impl epi::App for WarbossWaaghitApp {
             });
         });
 
-        egui::CentralPanel::default().show(ctx, |_ui| {
-            // ui.heading("egui template");
-            // ui.hyperlink("https://github.com/emilk/egui_template");
-            // ui.add(egui::github_link_file_line!(
-            //     "https://github.com/emilk/egui_template/blob/master/",
-            //     "Direct link to source code."
-            // ));
-            //egui::warn_if_debug_build(ui);
+        egui::CentralPanel::default().show(ctx, |ui| {
 
-            // ui.separator();
-            //
-            // ui.heading("Central Panel");
-            // ui.label("The central panel the region left after adding TopPanel's and SidePanel's");
-            // ui.label("It is often a great place for big things, like drawings:");
-            //
-            // ui.heading("Draw with your mouse to paint:");
-            // painting.ui_control(ui);
-            // egui::Frame::dark_canvas(ui.style()).show(ui, |ui| {
-            //     painting.ui_content(ui);
-            // });
+
+            egui::CollapsingHeader::new("Army Setup Path")
+                .default_open(army_setups_folder.show)
+                .show(ui, |ui| {
+                    ui.horizontal(|ui| {
+                        ui.label("File Path: ");
+                        ui.text_edit_singleline(&mut army_setups_folder.army_setups_folder);
+                    });
+
+
+                    if ui.button("Load Builds From Folder").clicked() {
+                        match validate_folder(& army_setups_folder.army_setups_folder){
+                            Some(is_army_setups_folder) => {
+                                if is_army_setups_folder {
+                                    //load_army_builds(army_setups_folder.army_setups_folder.as_str());
+                                }
+                            }
+                            None => {
+                                println!("error opening file");
+                            }
+                        }
+                        println!("todo folder reader function");
+                    }
+
+
+                    egui::CollapsingHeader::new("Hint")
+                        .default_open(false)
+                        .show(ui, |ui| {
+                            ui.label("This folder can be found in your TWW2 Roaming App Data folder ex:");
+                            ui.label("C:\\Users\\DaBiggestBoss\\AppData\\Roaming\\The Creative Assembly\\Warhammer2\\army_setups");
+                        });
+                });
+
+            ui.heading("Army Picker");
+            ui.horizontal(|ui| {
+                ui.label("Selected ");
+                ui.label(army_picker.selected_army_build.file_name.as_str());
+            });
+
+            ui.horizontal(|ui| {
+                if ui.button("Insert Build as ").clicked() {
+                    println!("todo make insert function")
+                }
+                ui.text_edit_singleline(&mut army_picker.insert_name);
+            });
+
+
+            //ARMIES Section
+            ui.heading("Armies");
+            ui.label("This shows how you can scroll to a specific item or pixel offset");
+
         });
 
         if false {
@@ -107,9 +140,10 @@ impl epi::App for WarbossWaaghitApp {
 
 
 // ----------------------------------------------------------------------------
+
 #[cfg_attr(feature = "persistence", derive(serde::Deserialize, serde::Serialize))]
 struct ArmySetupsFolder {
-    army_setup_folder: String,
+    army_setups_folder: String,
     valid_folder: bool,
     show: bool
 }
@@ -117,7 +151,7 @@ struct ArmySetupsFolder {
 impl Default for ArmySetupsFolder {
     fn default() -> Self {
         Self {
-            army_setup_folder: "C:\\Users\\DaBiggestBoss\\AppData\\Roaming\\The Creative Assembly\\Warhammer2\\army_setups".to_string(),
+            army_setups_folder: "C:\\Users\\DaBiggestBoss\\AppData\\Roaming\\The Creative Assembly\\Warhammer2\\army_setups".to_string(),
             valid_folder: false,
             show: true
         }
@@ -176,7 +210,7 @@ impl Default for ArmyPicker {
     fn default() -> Self {
         Self {
             selected_army_build: ArmyBuild::default(),
-            insert_name: "AAAAAAGH_WAAAAAAA".to_owned(),
+            insert_name: "AAAAAGGGHHWWWAAAAAAA".to_owned(),
             search_string: "".to_owned(),
             army_builds: vec![],
             displayed_builds: vec![]
@@ -186,7 +220,7 @@ impl Default for ArmyPicker {
 
 
 
-pub fn _validate_folder(folder_path: &str) -> Option<bool>{
+pub fn validate_folder(folder_path: &str) -> Option<bool>{
     println!("into validate folder");
     let path = std::path::Path::new(folder_path);
     if ! path.exists() {
@@ -210,7 +244,7 @@ pub fn _validate_folder(folder_path: &str) -> Option<bool>{
             println!("skip sub folder");
             continue
         } else {
-            if _is_army_setup_bool(&entry){
+            if is_army_setup_bool(&entry){
                 return Some(true);
             }
         }
@@ -220,11 +254,11 @@ pub fn _validate_folder(folder_path: &str) -> Option<bool>{
 }
 
 
-pub fn _load_army_builds(_folder_path: &str) -> Vec<ArmyBuild>{
+pub fn load_army_builds(_folder_path: &str) -> Vec<ArmyBuild>{
     vec![]
 }
 
-fn _is_army_setup_bool(file: &DirEntry) -> bool {
+fn is_army_setup_bool(file: &DirEntry) -> bool {
     match file.file_type(){
         Ok(ft) => {if  ft.is_file(){
             match  file.path().extension().and_then(OsStr::to_str){
@@ -243,13 +277,13 @@ fn _is_army_setup_bool(file: &DirEntry) -> bool {
 
 
 // the use for this is a directory search
-fn _visit_dirs(dir: &Path, cb: &dyn Fn(&DirEntry) -> bool) -> io::Result<bool> {
+fn visit_dirs(dir: &Path, cb: &dyn Fn(&DirEntry) -> bool) -> io::Result<bool> {
     if dir.is_dir() {
         for entry in fs::read_dir(dir)? {
             let entry = entry?;
             let path = entry.path();
             if path.is_dir() {
-                _visit_dirs(&path, cb)?;
+                visit_dirs(&path, cb)?;
             } else {
                 cb(&entry);
             }
