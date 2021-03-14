da59455d33034470d07f86e8c55fb45535258a4c
Jacob <bayswaterpc@gmail.com>
2021-03-14 18:01:22 -0500

    Add in warboss functions

--- src/app.rs
+++ src/app.rs
@@ -1,17 +1,21 @@
 use eframe::{egui, epi};
+use std::io;
+use std::fs::{self, DirEntry};
+use std::path::Path;
+use std::ffi::OsStr;
 
 /// We derive Deserialize/Serialize so we can persist app state on shutdown.
 #[cfg_attr(feature = "persistence", derive(serde::Deserialize, serde::Serialize))]
 pub struct WarbossWaaghitApp {
-    // Example stuff:
-    label: String
+    army_setups_folder: ArmySetupsFolder,
+    army_picker: ArmyPicker
 }
 
 impl Default for WarbossWaaghitApp {
     fn default() -> Self {
         Self {
-            // Example stuff:
-            label: "ello Waaaagh!".to_owned()
+            army_setups_folder: Default::default(),
+            army_picker: Default::default()
         }
     }
 }
@@ -37,7 +41,8 @@ impl epi::App for WarbossWaaghitApp {
     /// Put your widgets into a `SidePanel`, `TopPanel`, `CentralPanel`, `Window` or `Area`.
     fn update(&mut self, ctx: &egui::CtxRef, frame: &mut epi::Frame<'_>) {
         let WarbossWaaghitApp {
-            label,
+            army_setups_folder,
+            army_picker
         } = self;
 
         // Examples of how to create different panels and windows.
@@ -48,10 +53,6 @@ impl epi::App for WarbossWaaghitApp {
         egui::SidePanel::left("side_panel", 200.0).show(ctx, |ui| {
             ui.heading("Side Panel");
 
-            ui.horizontal(|ui| {
-                ui.label("Write something: ");
-                ui.text_edit_singleline(label);
-            });
 
             ui.with_layout(egui::Layout::bottom_up(egui::Align::Center), |ui| {
                 ui.add(
@@ -103,3 +104,158 @@ impl epi::App for WarbossWaaghitApp {
         }
     }
 }
+
+
+// ----------------------------------------------------------------------------
+#[cfg_attr(feature = "persistence", derive(serde::Deserialize, serde::Serialize))]
+struct ArmySetupsFolder {
+    army_setup_folder: String,
+    valid_folder: bool,
+    show: bool
+}
+
+impl Default for ArmySetupsFolder {
+    fn default() -> Self {
+        Self {
+            army_setup_folder: "C:\\Users\\DaBiggestBoss\\AppData\\Roaming\\The Creative Assembly\\Warhammer2\\army_setups".to_string(),
+            valid_folder: false,
+            show: true
+        }
+    }
+}
+
+#[cfg_attr(feature = "persistence", derive(serde::Deserialize, serde::Serialize))]
+pub enum Faction {
+    BM,
+    BRT,
+    CH,
+    DE,
+    DW,
+    EMP,
+    GS,
+    HE,
+    LM,
+    NRS,
+    SKV,
+    TK,
+    VC,
+    VP,
+    WE,
+    UNKNOWN,
+    ALL
+}
+
+#[cfg_attr(feature = "persistence", derive(serde::Deserialize, serde::Serialize))]
+pub struct ArmyBuild {
+    file_name: String,
+    faction: Faction,
+    vs_faction: Faction
+}
+
+impl Default for ArmyBuild {
+    fn default() -> Self {
+        Self {
+            file_name: "".to_owned(),
+            faction: Faction::UNKNOWN,
+            vs_faction: Faction::UNKNOWN
+        }
+    }
+}
+
+
+#[cfg_attr(feature = "persistence", derive(serde::Deserialize, serde::Serialize))]
+struct ArmyPicker {
+    selected_army_build: ArmyBuild,
+    insert_name: String,
+    search_string: String,
+    army_builds: Vec<ArmyBuild>,
+    displayed_builds: Vec<ArmyBuild>
+}
+
+impl Default for ArmyPicker {
+    fn default() -> Self {
+        Self {
+            selected_army_build: ArmyBuild::default(),
+            insert_name: "AAAAAAGH_WAAAAAAA".to_owned(),
+            search_string: "".to_owned(),
+            army_builds: vec![],
+            displayed_builds: vec![]
+        }
+    }
+}
+
+
+
+pub fn _validate_folder(folder_path: &str) -> Option<bool>{
+    println!("into validate folder");
+    let path = std::path::Path::new(folder_path);
+    if ! path.exists() {
+        return Some(false);
+
+    }
+    if ! path.is_dir() {
+        return Some(false);
+    }
+
+    if !folder_path.contains("The Creative Assembly") ||
+        !folder_path.contains("army_setups"){
+        println!("Wrong Folder");
+        return Some(false);
+    }
+
+    //make sure there are .army_setup files in the directory
+    for entry in fs::read_dir(path).unwrap() {
+        let entry = entry.unwrap();
+        if entry.path().is_dir() {
+            println!("skip sub folder");
+            continue
+        } else {
+            if _is_army_setup_bool(&entry){
+                return Some(true);
+            }
+        }
+    }
+
+    Some(false)
+}
+
+
+pub fn _load_army_builds(_folder_path: &str) -> Vec<ArmyBuild>{
+    vec![]
+}
+
+fn _is_army_setup_bool(file: &DirEntry) -> bool {
+    match file.file_type(){
+        Ok(ft) => {if  ft.is_file(){
+            match  file.path().extension().and_then(OsStr::to_str){
+                None => { return false;}
+                Some(ext) => {
+                    if ext == "army_setup" {
+                        return true;
+                    }
+                }
+            }
+        }}
+        Err(_) => { return false; }
+    }
+    false
+}
+
+
+// the use for this is a directory search
+fn _visit_dirs(dir: &Path, cb: &dyn Fn(&DirEntry) -> bool) -> io::Result<bool> {
+    if dir.is_dir() {
+        for entry in fs::read_dir(dir)? {
+            let entry = entry?;
+            let path = entry.path();
+            if path.is_dir() {
+                _visit_dirs(&path, cb)?;
+            } else {
+                cb(&entry);
+            }
+        }
+    }
+    Ok(true)
+}
+
+
