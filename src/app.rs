71246890bf9e47574fc2c794bcd44116e05aeffb
Jacob <bayswaterpc@gmail.com>
2021-03-14 17:49:57 -0500

    ello waaaaagh
    Parse down to essentials

--- src/app.rs
+++ src/app.rs
@@ -4,18 +4,14 @@ use eframe::{egui, epi};
 #[cfg_attr(feature = "persistence", derive(serde::Deserialize, serde::Serialize))]
 pub struct WarbossWaaghitApp {
     // Example stuff:
-    label: String,
-    value: f32,
-    painting: Painting,
+    label: String
 }
 
 impl Default for WarbossWaaghitApp {
     fn default() -> Self {
         Self {
             // Example stuff:
-            label: "Hello World!".to_owned(),
-            value: 2.7,
-            painting: Default::default(),
+            label: "ello Waaaagh!".to_owned()
         }
     }
 }
@@ -42,8 +38,6 @@ impl epi::App for WarbossWaaghitApp {
     fn update(&mut self, ctx: &egui::CtxRef, frame: &mut epi::Frame<'_>) {
         let WarbossWaaghitApp {
             label,
-            value,
-            painting,
         } = self;
 
         // Examples of how to create different panels and windows.
@@ -58,11 +52,6 @@ impl epi::App for WarbossWaaghitApp {
                 ui.label("Write something: ");
                 ui.text_edit_singleline(label);
             });
-
-            ui.add(egui::Slider::f32(value, 0.0..=10.0).text("value"));
-            if ui.button("Increment").clicked() {
-                *value += 1.0;
-            }
 
             ui.with_layout(egui::Layout::bottom_up(egui::Align::Center), |ui| {
                 ui.add(
@@ -82,26 +71,26 @@ impl epi::App for WarbossWaaghitApp {
             });
         });
 
-        egui::CentralPanel::default().show(ctx, |ui| {
-            ui.heading("egui template");
-            ui.hyperlink("https://github.com/emilk/egui_template");
-            ui.add(egui::github_link_file_line!(
-                "https://github.com/emilk/egui_template/blob/master/",
-                "Direct link to source code."
-            ));
-            egui::warn_if_debug_build(ui);
+        egui::CentralPanel::default().show(ctx, |_ui| {
+            // ui.heading("egui template");
+            // ui.hyperlink("https://github.com/emilk/egui_template");
+            // ui.add(egui::github_link_file_line!(
+            //     "https://github.com/emilk/egui_template/blob/master/",
+            //     "Direct link to source code."
+            // ));
+            //egui::warn_if_debug_build(ui);
 
-            ui.separator();
-
-            ui.heading("Central Panel");
-            ui.label("The central panel the region left after adding TopPanel's and SidePanel's");
-            ui.label("It is often a great place for big things, like drawings:");
-
-            ui.heading("Draw with your mouse to paint:");
-            painting.ui_control(ui);
-            egui::Frame::dark_canvas(ui.style()).show(ui, |ui| {
-                painting.ui_content(ui);
-            });
+            // ui.separator();
+            //
+            // ui.heading("Central Panel");
+            // ui.label("The central panel the region left after adding TopPanel's and SidePanel's");
+            // ui.label("It is often a great place for big things, like drawings:");
+            //
+            // ui.heading("Draw with your mouse to paint:");
+            // painting.ui_control(ui);
+            // egui::Frame::dark_canvas(ui.style()).show(ui, |ui| {
+            //     painting.ui_content(ui);
+            // });
         });
 
         if false {
@@ -112,77 +101,5 @@ impl epi::App for WarbossWaaghitApp {
                 ui.label("You would normally chose either panels OR windows.");
             });
         }
-    }
-}
-
-// ----------------------------------------------------------------------------
-
-/// Example code for painting on a canvas with your mouse
-#[cfg_attr(feature = "persistence", derive(serde::Deserialize, serde::Serialize))]
-struct Painting {
-    lines: Vec<Vec<egui::Pos2>>,
-    stroke: egui::Stroke,
-}
-
-impl Default for Painting {
-    fn default() -> Self {
-        Self {
-            lines: Default::default(),
-            stroke: egui::Stroke::new(1.0, egui::Color32::LIGHT_BLUE),
-        }
-    }
-}
-
-impl Painting {
-    pub fn ui_control(&mut self, ui: &mut egui::Ui) -> egui::Response {
-        ui.horizontal(|ui| {
-            egui::stroke_ui(ui, &mut self.stroke, "Stroke");
-            ui.separator();
-            if ui.button("Clear Painting").clicked() {
-                self.lines.clear();
-            }
-        })
-        .response
-    }
-
-    pub fn ui_content(&mut self, ui: &mut egui::Ui) -> egui::Response {
-        use egui::emath::{Pos2, Rect, RectTransform};
-
-        let (mut response, painter) =
-            ui.allocate_painter(ui.available_size_before_wrap_finite(), egui::Sense::drag());
-
-        let to_screen = RectTransform::from_to(
-            Rect::from_min_size(Pos2::ZERO, response.rect.square_proportions()),
-            response.rect,
-        );
-        let from_screen = to_screen.inverse();
-
-        if self.lines.is_empty() {
-            self.lines.push(vec![]);
-        }
-
-        let current_line = self.lines.last_mut().unwrap();
-
-        if let Some(pointer_pos) = response.interact_pointer_pos() {
-            let canvas_pos = from_screen * pointer_pos;
-            if current_line.last() != Some(&canvas_pos) {
-                current_line.push(canvas_pos);
-                response.mark_changed();
-            }
-        } else if !current_line.is_empty() {
-            self.lines.push(vec![]);
-            response.mark_changed();
-        }
-
-        let mut shapes = vec![];
-        for line in &self.lines {
-            if line.len() >= 2 {
-                let points: Vec<Pos2> = line.iter().map(|p| to_screen * *p).collect();
-                shapes.push(egui::Shape::line(points, self.stroke));
-            }
-        }
-        painter.extend(shapes);
-
-        response
     }
 }
